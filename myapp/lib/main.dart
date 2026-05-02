942c570c31de63bc080d83137f322a165001d659
Prolesk <prolesk.dn@gmail.com>
2026-05-02 11:39:47 +0300

    Изменена логика инкримента

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,15 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/screens/main_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Инкремент',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrement() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _counter = 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Кофе машина', home: MainScreen());
+    return Scaffold(
+      appBar: AppBar(title: Text('Инкремент')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Значение инкремента:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(icon: Icon(Icons.remove), onPressed: _decrement),
+                IconButton(icon: Icon(Icons.add), onPressed: _increment),
+              ],
+            ),
+
+            TextButton(
+              onPressed: _reset,
+              child: Text('Сбросить', style: TextStyle(color: Colors.grey)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
