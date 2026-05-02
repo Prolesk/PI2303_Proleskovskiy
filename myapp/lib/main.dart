bfff94acb064dcfb0224eddc7b991110ce0d4240
Prolesk <prolesk.dn@gmail.com>
2026-05-02 12:36:58 +0300

    Списки

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,141 +1,78 @@
 import 'package:flutter/material.dart';
+import 'simple_list.dart';
+import 'infinity_list.dart';
+import 'infinity_math_list.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Лаба 4',
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: MyHomePage(),
+      title: 'Лабораторная работа 5',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+      ),
+      home: const MainMenu(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool isLiked = false;
-  int likes = 41;
-
-  void toggleLike() {
-    setState(() {
-      isLiked = !isLiked;
-      isLiked ? likes++ : likes--;
-    });
-  }
-
-  void showAction(String text) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(text), duration: Duration(seconds: 1)),
-    );
-  }
+class MainMenu extends StatelessWidget {
+  const MainMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Layout")),
-      body: SingleChildScrollView(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text(
+          'Список элементов',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: false,
+      ),
+      body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              'assets/images/orig.jpg',
-              width: double.infinity,
-              height: 200,
-              fit: BoxFit.cover,
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SimpleList()),
+                );
+              },
+              child: const Text('Простой список'),
             ),
-
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Общежитие №20",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          "Краснодар, ул. Калинина, 13",
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  GestureDetector(
-                    onTap: toggleLike,
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.favorite,
-                          color: isLiked ? Colors.red : Colors.grey,
-                        ),
-                        SizedBox(width: 5),
-                        Text("$likes"),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const InfinityList()),
+                );
+              },
+              child: const Text('Бесконечный список со строками'),
             ),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  buildButton(
-                    Icons.call,
-                    "Позвонить",
-                    () => showAction("Позвонить"),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const InfinityMathList(),
                   ),
-                  buildButton(
-                    Icons.near_me,
-                    "Маршрут",
-                    () => showAction("Маршрут"),
-                  ),
-                  buildButton(
-                    Icons.share,
-                    "Поделиться",
-                    () => showAction("Поделиться"),
-                  ),
-                ],
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                "Студенческий городок или так называемый кампус Кубанского ГАУ состоит из двадцати общежитий, в которых проживает более 8000 студентов, что составляет 96% от всех нуждающихся. Студенты первого курса обеспечены местами в общежитии полностью. В соответствии с Положением о студенческих общежитиях университета, при поселении между администрацией и студентами заключается договор найма жилого помещения. Воспитательная работа в общежитиях направлена на улучшение быта, соблюдение правил внутреннего распорядка, отсутствия асоциальных явлений в молодежной среде. Условия проживания в общежитиях университетского кампуса полностью отвечают санитарным нормам и требованиям: наличие оборудованных кухонь, душевых комнат, прачечных, читальных залов, комнат самоподготовки, помещений для заседаний студенческих советов и наглядной агитации. С целью улучшения условий быта студентов активно работает система студенческого самоуправления - студенческие советы организуют всю работу по самообслуживанию",
-                textAlign: TextAlign.justify,
-              ),
+                );
+              },
+              child: const Text('Бесконечный список со степенями'),
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  Widget buildButton(IconData icon, String label, VoidCallback onTap) {
-    return InkWell(
-      onTap: onTap,
-      borderRadius: BorderRadius.circular(10),
-      child: Column(
-        children: [
-          Icon(icon, color: Colors.green),
-          SizedBox(height: 5),
-          Text(label, style: TextStyle(color: Colors.green)),
-        ],
       ),
     );
   }
